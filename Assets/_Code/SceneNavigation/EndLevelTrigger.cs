f0d2dec7855348b5675df0cd0b014da572d1f1e6
Aristarkh Abramovsky <ari.bram@ya.ru>
2023-03-18 15:06:57 +0300

    Triggers logic

--- Assets/_Code/SceneNavigation/EndLevelTrigger.cs
+++ Assets/_Code/SceneNavigation/EndLevelTrigger.cs
@@ -3,38 +3,11 @@ using System.Collections.Generic;
 using _Code.Services.Interfaces;
 using UnityEngine;
 
-public class EndLevelTrigger : MonoBehaviour, ILevelTrigger
+public class EndLevelTrigger : LevelTrigger
 {
-    [SerializeField]
-    private int level;
-
-    public void Init()
-    {
-
-    }
-
-    public void Init(ITriggerService triggerService)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public void EndLevel()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public void FailLevel()
-    {
-        throw new System.NotImplementedException();
-    }
-
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
-        {
-            //SceneController.NextLevel();
-        }
+        if (collision.CompareTag("Player")) EndLevel();
     }
-
 }
