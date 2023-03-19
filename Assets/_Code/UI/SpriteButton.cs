4938abfeb08e2ac34fc39146ee6334337016eb2c
pixel_error <90645177+wifilofi@users.noreply.github.com>
2023-03-19 12:38:49 +0300

    Add menu

--- Assets/_Code/UI/SpriteButton.cs
+++ Assets/_Code/UI/SpriteButton.cs
@@ -1,11 +1,36 @@
+using System;
+using UnityEngine;
+
 namespace _Code.UI
 {
     public class SpriteButton : ButtonBase
     {
-        
+        [Header("Sprites")]
+        [SerializeField] private Sprite _idleSprite;
+        [SerializeField] private Sprite _howerSprite;
+
+        [Header("Components")] 
+        [SerializeField] private SpriteRenderer _renderer;
+
+        private void OnEnable()
+        {
+            _renderer.sprite = _idleSprite;
+        }
+
         protected void OnMouseDown()
         {
+            _renderer.sprite = _idleSprite;
             _onClick?.Invoke();
+        }
+
+        protected void OnMouseEnter()
+        {
+            _renderer.sprite = _howerSprite;
+        }
+
+        protected void OnMouseExit()
+        {
+            _renderer.sprite = _idleSprite;
         }
     }
 }
